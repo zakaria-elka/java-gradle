7ad2a912dfb1fc0f4cd55dfb6aa08524a329911a
zakaria-elka <zakaria.elkm@gmail.com>
2022-05-25 01:48:59 +0100

    java

---
+++ src/test/java/testclasspersonne.java
@@ -0,0 +1,16 @@
+public class testclasspersonne {
+    private String name;
+
+    public testclasspersonne(){
+
+    }
+
+    public void setName() {
+        this.name = "azouz";
+    }
+
+    public  void main(String[] args) {
+        System.out.println(this.name);
+    }
+
+}
